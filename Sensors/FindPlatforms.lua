460e4a6222a1171c590d64fbe305a8ed7e75ed5e
Vojguard <63375836+Vojguard@users.noreply.github.com>
2025-05-06 16:41:55 +0200

    6.5.

--- Sensors/FindPlatforms.lua
+++ Sensors/FindPlatforms.lua
@@ -14,19 +14,40 @@ function getInfo()
 	}
 end
 
+function pointDistance(a, b)
+    local dx = b.x - a.x
+    local dz = b.z - a.z
+    return math.sqrt(dx * dx + dz * dz)
+end
+
 return function()
     local hills = {}
-    local X = Game.mapSizeX / 64
-    local Z = Game.mapSizeZ / 64
-    local maxHeight = Spring.GetGroundExtremes[2]
-    local idx = 1
-    for i=1, X do
-        for j=1, Z do
-            local h = Spring.GetGroundHeight(i * 64, j * 64)
-            if h >= maxHeight do                
-                hills[idx] = (x = i * 64,z = j * 64)
+    local X = Game.mapSizeX
+    local Z = Game.mapSizeZ
+    local minHeight, maxHeight = Spring.GetGroundExtremes()
+    if maxHeight ~= nil then
+        local heighPoints = {}
+        local idx = 1
+        for i=1, X, 10 do
+            for j=1, Z, 10 do
+                local h = Spring.GetGroundHeight(i, j)
+                if h >= maxHeight then
+                    local point = Vec3(i, h, j)
+                                     
+                    hills[idx] = Vec3(i, h, j)
+                    idx = idx + 1
+                end
             end
         end
+        -- local hills = {}
+
+        --[[
+        for _, point in ipairs(heighPoints) do
+            local close = false
+        end
+        ]]
+        return hills
+    else
+        return {}
     end
-    return heights
 end
