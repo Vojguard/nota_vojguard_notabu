6afb8884ecd4d02948f31eb8150e479e58caf06b
Vojguard <jirka.kropac@gmail.com>
2025-07-20 18:51:25 +0200

    starting at nearest point

--- Commands/followCorridor.lua
+++ Commands/followCorridor.lua
@@ -27,10 +27,22 @@ function Run(self, units, parameter)
         -- check if unit has command queue
         local hasCmdQueue = Spring.GetUnitCommands(unitID, 1)[1] ~= nil
 
-        if not hasCmdQueue then            
+        if not hasCmdQueue then
+			local closestPoint = #corridor
+			local corridorPoint = corridor[closestPoint].position
+			
+			-- find nearest point
+			for i=#corridor - 1, 1, -1 do
+				local nextPoint = corridor[i].position
+				if (unitPosVec:Distance(nextPoint) < unitPosVec:Distance(corridorPoint)) then
+					corridorPoint = nextPoint
+					closestPoint = i
+				else break end
+			end
+			
             -- give order queue to move in the corridor through its points
-            for i=1, #corridor do
-                local corridorPoint = corridor[i].position
+            for j=closestPoint, #corridor do
+				corridorPoint = corridor[j].position				
                 Spring.GiveOrderToUnit(unitID, cmdID, corridorPoint:AsSpringVector(), {"shift"})
             end
         end
