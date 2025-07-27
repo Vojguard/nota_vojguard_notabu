da4bbd6eac96a99e05edbac3dee2be5b98e33164
Vojguard <63375836+Vojguard@users.noreply.github.com>
2025-07-27 22:35:08 +0200

    danger counter

--- Commands/followCorridor.lua
+++ Commands/followCorridor.lua
@@ -9,20 +9,42 @@ function getInfo()
 				componentType = "editBox",
 				defaultValue = "",
 			},
+			{
+				name = "cautionRange",
+				variableType = "expression",
+				componentType = "editBox",
+				defaultValue = "10"
+			},
 		}
 	}
 end
 
 function Run(self, units, parameter)
 	local corridor = parameter.corridor -- array of Vec3
+	local cautionRange = parameter.cautionRange
 	
 	-- pick the spring command implementing the move
-	local cmdID = CMD.FIGHT
+	local cmdFightID = CMD.FIGHT
+	local cmdMoveID = CMD.MOVE
+
 	
 	for idx, unitID in ipairs(units) do
 
+		local myTeam = Spring.GetUnitTeam(unitID)
         local unitPosX, unitPosY, unitPosZ = Spring.GetUnitPosition(unitID)
         local unitPosVec = Vec3(unitPosX, unitPosY, unitPosZ)
+
+		-- check surroundings
+		local unitsAround = Spring.GetUnitsInSphere(unitPosX, unitPosY, unitPosZ, cautionRange)
+		local dangerCounter = 0
+		for _, uID in ipairs(unitsAround) do
+			local unitTeam = Spring.GetUnitTeam(uID)
+			if Spring.AreTeamsAllied(myTeam, unitTeam) then
+				dangerCounter = dangerCounter - 1
+			else
+				dangerCounter = dangerCounter + 1
+			end
+		end
 
         -- check if unit has command queue
         local hasCmdQueue = Spring.GetUnitCommands(unitID, 1)[1] ~= nil
@@ -40,10 +62,13 @@ function Run(self, units, parameter)
 				else break end
 			end
 			
+			if dangerCounter > 0 and closestPoint > 1 then
+				closestPoint = closestPoint - 1
+			end
             -- give order queue to move in the corridor through its points
             for j=closestPoint, #corridor do
 				corridorPoint = corridor[j].position				
-                Spring.GiveOrderToUnit(unitID, cmdID, corridorPoint:AsSpringVector(), {"shift"})
+                Spring.GiveOrderToUnit(unitID, cmdMoveID, corridorPoint:AsSpringVector(), {"shift"})
             end
         end
     end
