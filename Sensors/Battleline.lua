fab1c3215e5fe66800258772b1d90bcf8d8f08b1
Vojguard <jirka.kropac@gmail.com>
2025-08-06 22:03:00 +0200

    perp battleline

--- Sensors/Battleline.lua
+++ Sensors/Battleline.lua
@@ -21,9 +21,24 @@ function getInfo()
 	}
 end
 
+-- return a projection of the enemy position on the battlevec (intersection of two perpendicular vectors)
+function positionProjection(enemyPos, perpVec, basePos, battleVec) 
+    local dx = basePos.x - enemyPos.x
+    local dz = basePos.z - enemyPos.z
+
+    local det = perpVec.x * battleVec.z - perpVec.z * battleVec.x
+
+    local t = (dx * battleVec.z - dz * battleVec.x) / det
+    return {
+        x = enemyPos.x + t * perpVec.x,
+		y = 0,
+        z = enemyPos.z + t * perpVec.z
+    }
+end
 
 return function(corridor)
 	local strongpoints = Sensors.nota_vojguard_notabu.ReturnStrongpoints(corridor)
+	local basePos = strongpoints[1].position
     for spIdx = 1, #strongpoints, 1 do
 		local strongpointPos = strongpoints[spIdx].position
 		local units = Sensors.nota_vojguard_notabu.EnemyUnitsInArea(strongpointPos, 1500)
@@ -34,18 +49,32 @@ return function(corridor)
 				local unitPosX, unitPosY, unitPosZ = Spring.GetUnitPosition(units[unIdx])
         		local unitPosVec = Vec3(unitPosX, unitPosY, unitPosZ)
 
-				if (strongpointPos:Distance(closestUnit) > strongpointPos:Distance(unitPosVec)) then
+				if (basePos:Distance(closestUnit) > basePos:Distance(unitPosVec)) then
 					closestUnit = unitPosVec
 				end
 			end
-			local battleVec = closestUnit - strongpointPos
+			-- vector between both bases
+			local battleVec = strongpoints[#strongpoints].position - strongpoints[1].position 
+			battleVec:Normalize()
+			local perp = Vec3(-battleVec.z, battleVec.y, battleVec.x)
+			local conflictPoint = positionProjection(closestUnit, perp, basePos, battleVec)
+			conflictPoint.y = closestUnit.y
 			local battleline = {
-				['start'] = closestUnit + Vec3(-battleVec.x, battleVec.y, battleVec.z),
-				['end'] = closestUnit + Vec3(battleVec.x, battleVec.y, -battleVec.z)
+				['start'] = conflictPoint + perp * 500,
+				['conflict'] = conflictPoint,
+				['end'] = conflictPoint - perp * 500
 			}
 			if (Script.LuaUI('exampleDebug_update')) then
 				Script.LuaUI.exampleDebug_update(
 					spIdx,
+					{
+						startPos = conflictPoint - battleVec * 1000,
+						endPos = conflictPoint + battleVec * 100
+					}
+
+				)
+				Script.LuaUI.exampleDebug_update(
+					'battle',
 					{
 						startPos = battleline['start'],
 						endPos = battleline['end']
